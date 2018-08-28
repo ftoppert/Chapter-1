902f2a4d0e999b400f2ee68b0107e8a22137e6d2
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-08-28 09:17:51 -0500

    Sammysmotto2

---
+++ Exercises/src/Sammysmotto2/Sammysmotto2.java
@@ -0,0 +1,16 @@
+package Sammysmotto2;
+
+public class Sammysmotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs"); 
+		 System.out.println("Ss                                    Ss");
+		 System.out.println("Ss                                    Ss");
+		 System.out.println("Ss  Sammy's makes it fun in the sun   Ss");
+		 System.out.println("Ss                                    Ss");
+		 System.out.println("Ss                                    Ss");
+		 System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+	} 
+
+}
