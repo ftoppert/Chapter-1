b15e0de05627dddd5224b234f956fe3e3626e612
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-08-28 09:27:25 -0500

    Carly'sMotto

---
+++ Exercises/src/CarlysMotto/CarlysMotto.java
@@ -0,0 +1,10 @@
+package CarlysMotto;
+
+public class CarlysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Carly's makes the food that makes it a party.");
+	}
+
+}
