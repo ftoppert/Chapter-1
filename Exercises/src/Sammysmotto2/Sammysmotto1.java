66360f1559ecacfac70620791bd12c36e1b31c5b
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-08-28 09:21:15 -0500

    Sammysmotto1

---
+++ Exercises/src/Sammysmotto2/Sammysmotto1.java
@@ -0,0 +1,10 @@
+package Sammysmotto2;
+
+public class Sammysmotto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Sammy's makes it fun in the sun");
+	}
+
+}
