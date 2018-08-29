09486b92c0b233c68540aa9b8f1186cf556e7d21
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-08-29 09:41:01 -0500

    Chapter 2

---
+++ Chapter 2/src/youDoit/IntegerDemo.java
@@ -0,0 +1,20 @@
+package youDoit;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int anInt = 1234;
+		byte aByte = 12;
+		short aShort = 12;
+		long aLong = 1234567890987654321L;
+		double anotherInt = anInt * 1000000;
+		
+		System.out.println(" The int is " + anInt );
+		System.out.println(" The byte is " + aByte );
+		System.out.println(" The short is " + aShort );
+		System.out.println(" The long is " + aLong );
+		System.out.println(" Another int is " +anotherInt); 
+	}
+
+}
