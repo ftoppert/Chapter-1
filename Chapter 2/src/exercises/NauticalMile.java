e0314eeb24fb6cfedfbc93a07f4d4e4e2b5d7850
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-05 09:49:00 -0500

    NauticalMiles

---
+++ Chapter 2/src/exercises/NauticalMile.java
@@ -0,0 +1,25 @@
+package exercises;
+
+public class NauticalMile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double nauticalMilesToMilesConversion;
+		double nauticalMilesToKilometersConversion;
+		double kilometersToNauticalMiles= 1.852;
+		double milesToNauticalMiles =  1.150779;
+		double nauticalMilesEntered = 20;
+		
+		System.out.println("The conversion of Natical Miles to Miles is " +  nauticalMilesToMilesConversion);
+		System.out.println("The conversion of Nautical miles to Kilometers is" + nauticalMilesToKilometersConversion);
+	
+		
+		 
+	
+		
+		
+
+	}
+
+}
