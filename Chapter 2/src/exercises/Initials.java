e5ff1e21afed9cce026efb1f0e21c5556379acfe
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-06 09:22:02 -0500

    Initials

---
+++ Chapter 2/src/exercises/Initials.java
@@ -0,0 +1,13 @@
+package exercises;
+
+import javax.swing.JOptionPane;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" F. I. T.");
+	}
+
+}
