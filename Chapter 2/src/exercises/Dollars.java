55174560f323240b780ee355cf73cf189757b52b
ftoppert <42647326+ftoppert@users.noreply.github.com>
2018-09-07 09:51:54 -0500

    DollarsUnfinished

--- Chapter 2/src/exercises/Dollars.java
+++ Chapter 2/src/exercises/Dollars.java
@@ -1,10 +1,30 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 public class Dollars {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 class CountChange {
+		       
+	 double quarters= .25;   
+	 double  dimes= .10;      
+     double nickels= .05;   
+     double pennies= .01;    	          
+	 double dollars= 1;
+	  JOptionPane.showMessageDialog("Enter the number of quarters: ", JOptionPane.QUESTION_MESSAGE);
+	quarters: input.next();
+	  JOptionPane.showMessageDialog("Enter the number of dimes:     ", JOptionPane.QUESTION_MESSAGE);
+    dime:input.next();
+	  JOptionPane.showMessageDialog("Enter the number of nickels:   ", JOptionPane.QUESTION_MESSAGE);
+	nickel: input.next();
+	  JOptionPane.showMessageDialog("Enter the number of pennies:   ",JOptionPane.QUESTION_MESSAGE);
+	pennie: input.next();
+	  JOptionPane.showMessageDialog("The total in dollars is $" + dollars, JOptionPane.QUESTION_MESSAGE);
+	dollars: input.next();  
+		 
 
-	}
+}		
 
-}
+	
